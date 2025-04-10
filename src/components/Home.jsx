05bdf2371883feac3213a2950a79bbd0bc40bb37
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2025-04-11 00:03:40 +0530

    chanegs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,15 +18,9 @@ const Home = () => {
       {/* left side */}
       <div className='md:w-1/2 w-full space-y-8 mb-5'>
         <h2 className='my-8 lg:text-4xl text-3xl font-medium text-[#333] md:w-4/6 leading-normal mb-3'>Learn new concepts for each question</h2>
-        <p className='py-2 mb-6 text-gray-500 pl-2 border-l-4 border-indigo-700 text-base'>We help you prepare for exams and quizzes</p>
+        <p className='py-2 mb-6 text-gray-500 pl-2 border-l-4 border-indigo-700 text-base'>We are from Blind Code Team, Quest CSE, JNTUH</p>
         <div className='text-lg font-medium flex flex-col sm:flex-row gap-5'>
           <button onClick={handleStateQuiz} className='bg-primary px-6 py-2 text-white rounded'>Start Quiz</button>
-          <button className='inline-flex items-center  px-6 py-2 rounded border text-primary ml-3 hover:bg-primary hover:text-white transition-all duration-300 ease-in'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-            </svg>
-            Know more
-          </button>
         </div>
       </div>
       {/* right side */}
